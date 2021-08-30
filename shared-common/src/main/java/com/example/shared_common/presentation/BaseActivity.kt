ab664a30cef5398d4bc7edbff5a53a25cd158163
Wallace Baldenebre <wallacecorin@gmail.com>
2021-08-30 20:21:34 -0300

    atualizando biblioteca do coroutines e removendo classe nao usada

--- shared-common/src/main/java/com/example/shared_common/presentation/BaseActivity.kt
+++ shared-common/src/main/java/com/example/shared_common/presentation/BaseActivity.kt
@@ -1,8 +1,6 @@
 package com.example.shared_common.presentation
 
-import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
--- shared-common/src/main/java/com/example/shared_common/presentation/BaseNavigation.kt
+++
@@ -1,5 +0,0 @@
-package com.example.shared_common.presentation
-
-import org.koin.core.component.KoinComponent
-
-open class BaseNavigation : KoinComponent
