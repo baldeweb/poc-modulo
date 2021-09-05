0c423243f37c04b52dd113a4acd5008a354fe756
Wallace Baldenebre <wallacecorin@gmail.com>
2021-09-04 21:35:20 -0300

    troca do arquivo google services e remoçao dos modulos de testes do dynamicfeature

--- dynamicfeature/src/main/java/com/wallace/dynamicfeature/DynamicFeature.kt
+++
@@ -1,8 +0,0 @@
-package com.wallace.dynamicfeature
-
-import android.app.Activity
-import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
-import java.util.*
-
-class DynamicFeature: Activity() {
-}
